7e545a9ae157a8018a9d7d8374afc79bafcd108e
Anuranjit <anuranjit@bigbasket.com>
2016-11-11 00:34:15 +0530

    A

--- scripts/aerospike-client-c.sh
+++ scripts/aerospike-client-c.sh
@@ -68,7 +68,7 @@ detect_linux()
         return 0
         ;;
 
-      "ubuntu12" | "ubuntu13" | "ubuntu14" )
+      "ubuntu12" | "ubuntu13" | "ubuntu14" | "ubuntu16")
         echo "ubuntu12"  "deb"
         return 0
         ;;
